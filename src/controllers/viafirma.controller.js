8092e863359024b42d65cead947bac68afc3b2ea
Cristian <crisgues8@gmail.com>
2026-06-25 14:35:04 -0400

    Update: Added error handling to download request

--- src/controllers/viafirma.controller.js
+++ src/controllers/viafirma.controller.js
@@ -17,18 +17,24 @@ const recibirCallBackViaFirma = async (req, res) => {
                 headers: { Authorization: `Basic ${auth}` }
             }
         )
-        const downloadData = await response.json();
-        console.log("Download link obtenido: ", downloadData.link);
-        const pdfResponse = await fetch(downloadData.link, { method: 'GET' });
-        const arrayBuffer = await pdfResponse.arrayBuffer();
-        const pdfBuffer = Buffer.from(arrayBuffer);
 
-        const dataRecordId = await createDataRecord(messageCode);
-        const uploadResult = await uploadDocument(dataRecordId, pdfBuffer);
-        console.log("Resultado del upload a DocuWare: ", uploadResult);
-        
+        if (!response.ok) {
+            const errorText = await response.text();
+            console.error(`Error al descargar el documento firmado: ${response.status} - ${errorText}`);
+            return res.status(response.status).json({ error: `Error al descargar el documento firmado: ${errorText}` });
+        }
+        else {
+            const downloadData = await response.json();
+            console.log("Download link obtenido: ", downloadData.link);
+            const pdfResponse = await fetch(downloadData.link, { method: 'GET' });
+            const arrayBuffer = await pdfResponse.arrayBuffer();
+            const pdfBuffer = Buffer.from(arrayBuffer);
+
+            const dataRecordId = await createDataRecord(messageCode);
+            const uploadResult = await uploadDocument(dataRecordId, pdfBuffer);
+            console.log("Resultado del upload a DocuWare: ", uploadResult);
+        }
     }
-    
     return res.status(200).json({ message: "Callback recibido correctamente", data: viaFirmaCBResponse });
 }
 
